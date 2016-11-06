737a2b6f72e9b01680f27a0fc057324b3fbe3629
Goto01 <kirill.tkachev.ktkach@gmail.com>
2016-11-06 14:38:22 +0300

    + create new item in the ManageItemsController

--- shop/src/main/java/com/tofi/shop/controller/ManageItemsController.java
+++ shop/src/main/java/com/tofi/shop/controller/ManageItemsController.java
@@ -1,13 +1,34 @@
 package com.tofi.shop.controller;
 
+import com.tofi.shop.domain.Item;
+import com.tofi.shop.service.ItemService;
+import com.tofi.shop.service.ServiceException;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
 
 @Controller
 @RequestMapping("admin/manage-items")
 public class ManageItemsController {
+    private ItemService itemService;
+
+    @Inject
+    ManageItemsController(ItemService itemService){
+        Assert.notNull(itemService, "ItemService must be not null");
+        this.itemService = itemService;
+    }
+
     @RequestMapping("")
     public String index(){
+        return "admin-manage";
+    }
+
+    @RequestMapping("/add")
+    public String addItem(@ModelAttribute("newItem") Item item) throws ServiceException {
+        itemService.create(item);
         return "admin-manage";
     }
 }
